ed7b850586c3d4c79095cc0b56240c9ed8537a23
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-03-13 13:47:16 +0500

    update

--- src/Components/Sheduler.jsx
+++ src/Components/Sheduler.jsx
@@ -81,7 +81,7 @@ const Form = ({ selected, setselected, setsubmitted }) => {
         "-" +
         selectedDate.getFullYear(),
     };
-    await fetch("http://localhost:3000/api/email", {
+    await fetch("https://kabariya.pk/api/email", {
       body: JSON.stringify(details),
       method: "POST",
     });
