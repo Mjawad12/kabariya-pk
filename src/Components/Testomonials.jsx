182b33d12a1e7be6786b62272a68a50537ea89d6
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-05-22 19:38:01 +0500

    crausalCardFix

--- src/Components/Testomonials.jsx
+++ src/Components/Testomonials.jsx
@@ -99,11 +99,11 @@ const CarasouelCard = ({
 }) => {
   return (
     <div
-      className={`card max-w-[307px] small:max-w-[290px] w-full m-auto gap-4 border mx-[15px] small:mx-0 shadow-sm
+      className={`card min-w-[307px] small:max-w-[290px] w-full m-auto gap-4 border mx-[15px] small:mx-0 shadow-sm
        border-[#DADADA] h-[480px] small:h-[435px] border-dashed rounded-[24.54px] hover:border-[#0000006f]
     flex flex-col justify-start items-start px-8 pr-7 py-[6rem] pb-[1rem] relative bg-white`}
     >
-      <div className="cardPic rounded-full  w-[117px] h-[117px] absolute top-[-50px] flex justify-center items-center border bg-white border-[#DADADA] border-dashed ">
+      <div className="cardPic rounded-full w-[117px] h-[117px] absolute top-[-50px] flex justify-center items-center border bg-white border-[#DADADA] border-dashed ">
         <Image
           src={reviewPage ? image : `/Testomonials/${index + 1}.webp`}
           width={5000}
